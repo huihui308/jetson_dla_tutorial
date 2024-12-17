e7025d2508a6ac8076c7b9f736f86c46b3de51d0
david <305573571@qq.com>
2024-12-17 16:23:57 +0800

    add requirements.txt

---
+++ david.md
@@ -0,0 +1,27 @@
+
+Install envirment:
+```
+$ pip install onnx-graphsurgeon -i https://pypi.tuna.tsinghua.edu.cn/simple
+```
+
+
+
+
+
+
+
+
+
+python3 modify.py --input=./data/model_bn.onnx --output=./data/model_bn_modify.onnx
+
+
+
+
+
+
+
+
+
+
+
+
--- modify.py
+++ modify.py
@@ -8,13 +8,14 @@ import onnx
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('input', type=str, help='Path to the ONNX model.')
-parser.add_argument('output', type=str, help='Path to output the modified ONNX model.')
+parser.add_argument('--input', type=str, help='Path to the ONNX model.')
+parser.add_argument('--output', type=str, help='Path to output the modified ONNX model.')
 args = parser.parse_args()
 
 graph = gs.import_onnx(onnx.load(args.input))
 
 for node in graph.nodes:
+    # print(node.op)
     if node.op == 'GlobalAveragePool':
         node.op = 'AveragePool'
         node.attrs['kernel_shape'] = [2, 2]
